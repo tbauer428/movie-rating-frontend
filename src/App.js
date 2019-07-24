7bdda0f6ad4158f1318ba936d74f3fa9731c6174
ttjb2 <tom.bauer95@gmail.com>
2019-07-24 13:46:55 -0500

    initial commit"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount = () => {
+    axios.get("/movies").then(response =>
+      this.setState({
+        data: response.data
+      })
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.data.map(movie => {
+          return (
+            <div>
+              {movie.primaryTitle}, Runtime: {movie.runtimeMinutes}, Adults
+              Only: {movie.adult}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
